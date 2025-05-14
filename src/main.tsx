9312397a4a7b542c7818a96f3ae7ba993c5c5845
Adam Chain <achain123@gmail.com>
2025-05-13 20:57:16 -0400

    Fix Buffer import in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import App from './App';
 import './index.css';
 
 // Add Buffer polyfill
-import { Buffer } from 'buffer';
 globalThis.Buffer = Buffer;
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
